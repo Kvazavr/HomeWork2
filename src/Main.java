918ee492a6fc238e2ba898f4c5a7753040bd98e9
Elena Nekrasova <kvazavr@yandex.ru>
2023-02-10 11:54:19 +0400

    Reformat code

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int costTicket = 273;
         int mile = 20;
-        int bonus = costTicket/mile;
+        int bonus = costTicket / mile;
 
         System.out.println("Вам начислено " + bonus + " бонусных миль");
     }
